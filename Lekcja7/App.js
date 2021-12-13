0ae3a0c546527c803e096ef64ebcf9eed3e5eb8f
Oskar <oskiflog@gmail.com>
2021-12-13 20:27:12 +0100

    NotesApp: progress

---
+++ Lekcja7/App.js
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
+import Notes from './components/Notes';
+import AddNote from './components/AddNote';
+import Info from './components/Info';
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem label="info" onPress={() => alert('Link to help')} />
+    </DrawerContentScrollView>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+		<Drawer.Navigator drawerContent={(props) => <CustomDrawerContent {...props} />}>
+			<Drawer.Screen
+				options={{
+					drawerIcon: () => (
+						<Image
+							style={styles.icon}
+							source={require('./assets/notes.png')}
+						/>
+					)
+				}} 
+				name="Notes" 
+				component={Notes}
+			/>
+			<Drawer.Screen 
+				options={{
+					drawerIcon: () => (
+						<Image
+							style={styles.icon}
+							source={require('./assets/plus.png')}
+						/>
+					)
+				}} 
+				name="AddNote" 
+				component={AddNote} 
+			/>
+			<Drawer.Screen 
+				options={{
+					drawerIcon: () => (
+						<Image
+							style={styles.icon}
+							source={require('./assets/info.png')}
+						/>
+					)
+				}} 
+				name="Info" 
+				component={Info} 
+			/>
+		</Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+	icon: {
+		height: 30,
+		width: 30,
+	},
+})
